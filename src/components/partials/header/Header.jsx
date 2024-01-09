0aa2b314d79390c6f7fef5cff0d5c130590a0c40
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-09 20:55:24 +0530

    added styles to header

--- src/components/partials/header/Header.jsx
+++ src/components/partials/header/Header.jsx
@@ -1,23 +1,38 @@
 import headerStyles from "./Header.module.css";
 
+import { IconHelp, IconSearch } from "@tabler/icons-react";
+
 const Header = () => {
 	return (
 		<div className={headerStyles.container}>
 			<div className={headerStyles.headerLeft}>
 				<div className={headerStyles.pageTitle}>Payments</div>
 
-				<div className={headerStyles.pageHelp}>How it works</div>
+				<div className={headerStyles.pageHelp}>
+					<IconHelp size={15} />
+					How it works
+				</div>
 			</div>
 
 			<div className={headerStyles.searchBarContainer}>
-				<span className={headerStyles.searchIcon}></span>
-				<input type="text" className={headerStyles.searchInput} />
+				<span className={headerStyles.searchIcon}>
+					<IconSearch size={16} color="#808080" />
+				</span>
+				<input
+					type="text"
+					className={headerStyles.searchInput}
+					placeholder="Search features, tutorials, etc."
+				/>
 			</div>
 
 			<div className={headerStyles.headerRight}>
-				<div className={headerStyles.chatButton}>C</div>
+				<div className={headerStyles.headerRightIcon}>
+					<img src="headericon1.png" alt="icon1" />
+				</div>
 
-				<div className={headerStyles.dropDownBtn}>D</div>
+				<div className={headerStyles.headerRightIcon}>
+					<img src="headericon2.png" alt="icon2" />
+				</div>
 			</div>
 		</div>
 	);
