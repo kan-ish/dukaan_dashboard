65634ec15d49e539f0d01d1f5ebc7674dc75de9a
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-09 19:57:44 +0530

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Layout from "./components/partias/layout/Layout";
+import Layout from "./components/partials/layout/Layout";
 import Payments from "./components/payments/Payments";
 
 import "./App.css";
