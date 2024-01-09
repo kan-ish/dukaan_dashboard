5cd9de0510793ce4c7fcd4b481b49eeca500f942
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-09 18:11:19 +0530

    Added header component

--- src/components/partias/header/Header.jsx
+++ src/components/partias/header/Header.jsx
@@ -1,5 +1,26 @@
+import headerStyles from "./Header.module.css";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className={headerStyles.container}>
+			<div className={headerStyles.headerLeft}>
+				<div className={headerStyles.pageTitle}>Payments</div>
+
+				<div className={headerStyles.pageHelp}>How it works</div>
+			</div>
+
+			<div className={headerStyles.searchBarContainer}>
+				<span className={headerStyles.searchIcon}></span>
+				<input type="text" className={headerStyles.searchInput} />
+			</div>
+
+			<div className={headerStyles.headerRight}>
+				<div className={headerStyles.chatButton}>C</div>
+
+				<div className={headerStyles.dropDownBtn}>D</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
