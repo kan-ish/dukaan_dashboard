332c0d14d3831c11f22e4ced77c30a9e36a73ce4
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-10 00:01:51 +0530

    changes to gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/dukaan_dash/",
+	base: "/dukaan_dashboard/",
 	plugins: [react()],
 });
